00ae24c9c23c1f5ce52d7765e4bbca5ac669a8b9
goldsami <zolotarenkomax@gmail.com>
2024-08-27 16:38:00 +0300

    refactor: endpoint renamed

--- server/src/config/routes.ts
+++ server/src/config/routes.ts
@@ -4,8 +4,8 @@ import QuestionsCtrl from '../controllers/questionsCtrl';
 const router = express.Router();
 
 router.get('/questions', [], QuestionsCtrl.list);
-router.put('/add', [], QuestionsCtrl.add);
-router.post('/update/:id', [], QuestionsCtrl.update);
-router.delete('/delete/:id', [], QuestionsCtrl.delete);
+router.put('/questions', [], QuestionsCtrl.add);
+router.post('/questions/:id', [], QuestionsCtrl.update);
+router.delete('/questions/:id', [], QuestionsCtrl.delete);
 
 export default router;
